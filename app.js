8c9ef494de41a49be2f6ebbe7788c9e3aa0016d5
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-07 10:46:13 +0100

    Id

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ console.log(wrapper);
 const title = document.getElementsByClassName("title");
 console.log(title);
 console.log(Array.isArray(Array.from(title)))
+let head = document.getElementsByTagName("header");
+console.log(head);
+const bookList = document.querySelector("#book-list")
+console.log(bookList);
