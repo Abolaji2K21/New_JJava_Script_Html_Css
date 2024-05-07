c23eab50c4fb88e47fad85ab23e45e7bdd7495f7
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-07 10:34:37 +0100

    Okay

---
+++ app.js
@@ -0,0 +1,5 @@
+const wrapper = document.getElementById("wrapper");
+console.log(wrapper);
+const title = document.getElementsByClassName("title");
+console.log(title);
+console.log(Array.isArray(Array.from(title)))
