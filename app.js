78958faad5d3be22791539e1a93cf254d99585d5
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-07 11:19:16 +0100

    PenisGOne

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ console.log(Array.isArray(Array.from(title)))
 let head = document.getElementsByTagName("header");
 console.log(head);
 const bookList = document.querySelector("#book-list")
-console.log(bookList);
+console.log(bookList.textContent);
+const bookListOne = document.querySelectorAll("#book-list ul li");
+console.log(bookListOne[1].textContent);
+// bookListOne.forEach(book => {
+//     console.log(book.textContent);
+// })
+
+
+
